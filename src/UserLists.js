d0a8d892275cb1e34e9ea28a9284a027f53d6b7e
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2024-02-12 16:20:02 +0100

    Adicionando tokens e a api

--- src/UserLists.js
+++ src/UserLists.js
@@ -1,11 +1,29 @@
 import React from "react";
 import ListComponent from "./ListComponent";
 
-export default function UserLists(){
-    return (
-        <div>
-            <ListComponent listname={'Minha lista'} />
-            <ListComponent listname={'Minha lista 2'} />
-        </div>
-    )
+export default class UserLists extends React.Component{
+    state = { lists:null, loading: true }
+    async componentDidMount(){
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        config.headers['Authorization'] ='Token 99bac42d4f4ffde4b583bb1a8335a950219998de'
+        var url = 'http://127.0.0.1:8000/list/';
+        const response = await fetch(url, config);
+        const data = await response.json();
+        console.log(data)
+        this.setState({lists: data, loading: false});
+    }
+    render()
+    {
+        return (
+            <div>
+                <ListComponent listname={'Minha lista'} />
+                <ListComponent listname={'Minha lista 2'} />
+            </div>
+        )
+    }
+    
 }
